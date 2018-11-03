e0db2f710ef76db49350321edd62cfbe2a0ec9fe
mcdonl1 <lucmcd10@gmail.com>
2018-11-03 18:04:47 -0400

    added firebase init code

--- src/App.js
+++ src/App.js
@@ -26,3 +26,22 @@ class App extends Component {
 }
 
 export default App;
+
+
+// const firebase = require("firebase");
+// // Required for side-effects
+// require("firebase/firestore");
+
+// firebase.initializeApp({
+//   apiKey: '### FIREBASE API KEY ###',
+//   authDomain: '### FIREBASE AUTH DOMAIN ###',
+//   projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+// });
+
+// // Initialize Cloud Firestore through Firebase
+// var db = firebase.firestore();
+
+// // Disable deprecated features
+// db.settings({
+//   timestampsInSnapshots: true
+// });
