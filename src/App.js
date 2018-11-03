c2bf2fe08be1db4b07b294bb019c981a342488a7
Graeme Woods <graemewoods202@gmail.com>
2018-11-03 19:26:55 -0400

    Layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,97 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+function Leaderboard(props) {
+  return (
+    <table class="leaderboard">
+      <tr>
+        <th>Rank</th>
+        <th>Score</th>
+        <th>Name</th>
+      </tr>
+      {props.people.map(p =>
+        <tr>
+          <td>{p.rank}</td>
+          <td>{p.score}</td>
+          <td>{p.name}</td>
+        </tr>
+      )}
+    </table>
+  );
+}
+
+
+function Roles(props) {
+  return (
+    <table class="roles">
+      {props.people.map(p =>
+        <tr>
+          <td>{p.role}</td>
+          <td>{p.name}</td>
+        </tr>
+      )}
+    </table>
+  );
+}
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [
+        { rank: 1,
+          name: "Graeme",
+          score: 4
+        },
+        { rank: 2,
+          name: "Lucas",
+          score: 2
+        },
+        { rank: 3,
+          name: "Jack",
+          score: 1
+        },
+        { rank: 4,
+          name: "Andre",
+          score: 0
+        }],
+        roleOrder: [
+          { name: "Graeme",
+            role: "Counters"
+           },
+          { name: "Lucas",
+            role: "Dishes"
+           },
+          { name: "Jack",
+            role: "Floors"
+           },
+          { name: "Andre",
+            role: "Garbage"
+           }]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="header">
+        <h1>Title</h1>
+        <div class="row">
+          <div class="left">
+            <h2>Leaderboard</h2>
+            <Leaderboard people={this.state.board} />
+          </div>
+          <div class="right">
+            <h2>Jobs</h2>
+            <Roles people={this.state.roleOrder}/>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default Header;
+
 
 
 // const firebase = require("firebase");
