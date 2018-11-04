2bb933b0f1b17fc410276f4d7566f5c5a776f4f9
Graeme Woods <graemewoods202@gmail.com>
2018-11-04 00:35:08 -0400

    db access and moment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var moment = require('moment');
+
+const firebase = require('firebase/app');
+// Required for side-effects
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAlWvWD1ISY8c2_QOCZ7H_Spj9GlpiolP4",
+  authDomain: "emersonchores.firebaseapp.com",
+  projectId: "emersonchores"
+});
+
+// Initialize Cloud Firestore through Firebase
+const db = firebase.firestore();
+const settings = { timestampsInSnapshots: true};
+db.settings(settings);
+
+var usersC = db.collection('users');
+
+var dateC = db.collection('general').doc('lastUpdated');
+
 
 function Leaderboard(props) {
   return (
-    <table class="leaderboard">
-      <tr>
-        <th>Rank</th>
-        <th>Score</th>
-        <th>Name</th>
-      </tr>
-      {props.people.map(p =>
+    <table className="leaderboard">
+      <tbody>
         <tr>
-          <td>{p.rank}</td>
-          <td>{p.score}</td>
-          <td>{p.name}</td>
+          <th>Rank</th>
+          <th>Score</th>
+          <th>Name</th>
         </tr>
-      )}
+        { props.people.map( (p, index) =>
+          <tr key={p.id}>
+            <td>{index + 1}</td>
+            <td>{p.points}</td>
+            <td>{p.name}</td>
+          </tr>
+          )}
+      </tbody>
     </table>
   );
 }
@@ -24,13 +47,15 @@ function Leaderboard(props) {
 
 function Roles(props) {
   return (
-    <table class="roles">
-      {props.people.map(p =>
-        <tr>
-          <td>{p.role}</td>
-          <td>{p.name}</td>
-        </tr>
-      )}
+    <table className="roles">
+      <tbody>
+        {props.people.map(p =>
+          <tr key={p.id}>
+            <td>ROLL</td>
+            <td>{p.name}</td>
+          </tr>
+        )}
+      </tbody>
     </table>
   );
 }
@@ -39,75 +64,52 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: [
-        { rank: 1,
-          name: "Graeme",
-          score: 4
-        },
-        { rank: 2,
-          name: "Lucas",
-          score: 2
-        },
-        { rank: 3,
-          name: "Jack",
-          score: 1
-        },
-        { rank: 4,
-          name: "Andre",
-          score: 0
-        }],
-        roleOrder: [
-          { name: "Graeme",
-            role: "Counters"
-           },
-          { name: "Lucas",
-            role: "Dishes"
-           },
-          { name: "Jack",
-            role: "Floors"
-           },
-          { name: "Andre",
-            role: "Garbage"
-           }]
+      people: [],
+      lastUpdated: '',
+      offset: ''
     };
+
+    dateC.get()
+      .then(doc => {
+        var dbDate = moment(`${doc.data().date} 00:00:00`,"DD/MM/YYYY HH:mm:ss");
+        var ms = moment().diff(dbDate);
+        var offset = Math.floor(moment.duration(ms)._data.days/7);
+        if (offset > 0) {
+          doc.ref.update({'date': dbDate.add(offset, 'w').format("DD/MM/YYYY")});
+          //new week, inc by offset
+        }
+      })
+
+
+    usersC.orderBy('points', 'desc').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          var newArray = this.state.people.slice();
+          newArray.push(doc.data());
+          this.setState({people:newArray})
+        });
+      })
+      .catch(err => {
+        console.log('Error getting documents', err);
+      });
   }
   render() {
     return (
       <div className="header">
         <h1>Title</h1>
-        <div class="row">
-          <div class="left">
+        <div className="row">
+          <div className="left">
             <h2>Leaderboard</h2>
-            <Leaderboard people={this.state.board} />
+            <Leaderboard people={this.state.people} />
           </div>
-          <div class="right">
-            <h2>Jobs</h2>
-            <Roles people={this.state.roleOrder}/>
+          <div className="right">
+            <h2>Roles</h2>
+            <Roles people={this.state.people}/>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
 export default Header;
-
-
-
-// const firebase = require("firebase");
-// // Required for side-effects
-// require("firebase/firestore");
-
-// firebase.initializeApp({
-//   apiKey: '### FIREBASE API KEY ###',
-//   authDomain: '### FIREBASE AUTH DOMAIN ###',
-//   projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-// });
-
-// // Initialize Cloud Firestore through Firebase
-// var db = firebase.firestore();
-
-// // Disable deprecated features
-// db.settings({
-//   timestampsInSnapshots: true
-// });
