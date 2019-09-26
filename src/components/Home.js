c647108e10d07b478c3bb48c5c5f0bd024b8b042
Graeme Woods <graemewoods202@gmail.com>
2019-09-26 13:23:49 -0400

    remove leaderboard and fix bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import db from '../firebaseconfig';
 import Roles from './Roles';
 
@@ -8,166 +8,237 @@ const firebase = require('firebase/app');
 var usersC = db.collection('users');
 var dateC = db.collection('general').doc('lastUpdated');
 
-
 function Leaderboard(props) {
-  function addPoint(user, chore) {
-    db.collection("users").where("name", "==", user)
-    .get()
-    .then(function(querySnapshot) {
-
-        var doc = querySnapshot.docs[0];
-        var current = doc.data().points;
-        doc.ref.update({
-            points: current + 1
-        });
-
-    })
-    .catch(function(error) {
-        console.log("Error getting documents from users: ", error);
-    }).then(function() {
-        let date = moment().utc();
-        date = parseInt(date.format("YYYYMMDDHHmmss"));
-        let data = {
-          name: user,
-          chore: chore,
-          date: date
-        };
-        db.collection("eventLogData").add(data);
-    })
-    .catch(function(error) {
-        console.error("Error adding document to eventLogData: ", error);
-    });
-  }
-  return (
-    <table className="leaderboard">
-      <tbody>
-        <tr>
-          <th>Rank</th>
-          <th>Score</th>
-          <th>Name</th>
-        </tr>
-        { props.people
-          .sort(function compare(a, b) {
-            if (a.points < b.points) return 1;
-            if (a.points > b.points) return -1;
-            return 0;
-          })
-          .map( (p, index) =>
-          <tr key={p.id}>
-            <td>{index + 1}</td>
-            <td>{p.points}</td>
-            <td>{p.name}</td>
-              <td className="btn-container">
-                <button className="btn">Finshed Chore</button>
-                <ul className="dropdown">
-                  <li><button className="drop-btn" type="submit" onClick={() => {addPoint(p.name, 'counters')}}>Counters</button></li>
-                  <li><button className="drop-btn" type="submit" onClick={() => {addPoint(p.name, 'dishes')}}>Dish Unload</button></li>
-                  <li><button className="drop-btn" type="submit" onClick={() => {addPoint(p.name, 'garbage')}}>Garbage</button></li>
-                  <li><button className="drop-btn" type="submit" onClick={() => {addPoint(p.name, 'swept')}}>Sweep</button></li>
-                  <li><button className="drop-btn" type="submit" onClick={() => {addPoint(p.name, 'bathroom')}}>Bathroom</button></li>
-                </ul>
-              </td>
-          </tr>
-          )}
-      </tbody>
-    </table>
-  );
+	function addPoint(user, chore) {
+		db.collection('users')
+			.where('name', '==', user)
+			.get()
+			.then(function(querySnapshot) {
+				var doc = querySnapshot.docs[0];
+				var current = doc.data().points;
+				doc.ref.update({
+					points: current + 1,
+				});
+			})
+			.catch(function(error) {
+				console.log('Error getting documents from users: ', error);
+			})
+			.then(function() {
+				let date = moment().utc();
+				date = parseInt(date.format('YYYYMMDDHHmmss'));
+				let data = {
+					name: user,
+					chore: chore,
+					date: date,
+				};
+				db.collection('eventLogData').add(data);
+			})
+			.catch(function(error) {
+				console.error('Error adding document to eventLogData: ', error);
+			});
+	}
+	return (
+		<table className="leaderboard">
+			<tbody>
+				<tr>
+					<th>Rank</th>
+					<th>Score</th>
+					<th>Name</th>
+				</tr>
+				{props.people
+					.sort(function compare(a, b) {
+						if (a.points < b.points) return 1;
+						if (a.points > b.points) return -1;
+						return 0;
+					})
+					.map((p, index) => (
+						<tr key={p.id}>
+							<td>{index + 1}</td>
+							<td>{p.points}</td>
+							<td>{p.name}</td>
+							<td className="btn-container">
+								<button className="btn">Finshed Chore</button>
+								<ul className="dropdown">
+									<li>
+										<button
+											className="drop-btn"
+											type="submit"
+											onClick={() => {
+												addPoint(p.name, 'counters');
+											}}
+										>
+											Counters
+										</button>
+									</li>
+									<li>
+										<button
+											className="drop-btn"
+											type="submit"
+											onClick={() => {
+												addPoint(p.name, 'dishes');
+											}}
+										>
+											Dish Unload
+										</button>
+									</li>
+									<li>
+										<button
+											className="drop-btn"
+											type="submit"
+											onClick={() => {
+												addPoint(p.name, 'garbage');
+											}}
+										>
+											Garbage
+										</button>
+									</li>
+									<li>
+										<button
+											className="drop-btn"
+											type="submit"
+											onClick={() => {
+												addPoint(p.name, 'swept');
+											}}
+										>
+											Sweep
+										</button>
+									</li>
+									<li>
+										<button
+											className="drop-btn"
+											type="submit"
+											onClick={() => {
+												addPoint(p.name, 'bathroom');
+											}}
+										>
+											Bathroom
+										</button>
+									</li>
+								</ul>
+							</td>
+						</tr>
+					))}
+			</tbody>
+		</table>
+	);
 }
-
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      roles: [
-        { name: 'Counter Commander',
-          description: 'Keep counters and table clean and wipe stovetop if needed.'
-        },
-        { name: 'Dish Deputy',
-          description: "Make sure Billy's are doing their dishes and keeping sink clear. No need to do other's dishes."
-        },
-        { name: 'Garbage Governor',
-          description: 'Take out garbage and recycling each week.'
-        },
-        { name: 'Living Room Lieutenant',
-          description: 'Militantly enforce a clean living room table.'
-        },
-        { name: 'Sweeping Sergeant',
-          description: 'Sweep kitchen when needed.'
-        }
-      ],
-      people: []
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			roles: [
+				{
+					name: 'Counter Commander',
+					description:
+						'Keep counters and table clean and wipe stovetop if needed.',
+				},
+				{
+					name: 'Dish Deputy',
+					description:
+						"Make sure Billy's are doing their dishes and keeping sink clear. No need to do other's dishes.",
+				},
+				{
+					name: 'Garbage Governor',
+					description: 'Take out garbage with help from the Garbage Governor.',
+				},
+				{
+					name: 'Recycling Ranger',
+					description: 'Take out garbage with help from the Recycling Ranger.',
+				},
+				{
+					name: 'Living Room Lieutenant',
+					description: 'Militantly enforce a clean living room table.',
+				},
+				{
+					name: 'Sweeping Sergeant',
+					description: 'Sweep kitchen when needed.',
+				},
+			],
+			people: [],
+		};
 
-    dateC.get()
-      .then(doc => {
-        //account for timezone offset
-        var dbDate = moment("01/01/1970 00:00:00","DD/MM/YYYY HH:mm:ss").seconds(doc.data().date.seconds).add(-5, 'h');
-        var ms = moment().diff(dbDate);
-        var offset = Math.floor(moment.duration(ms)._data.days/7);
-        if (offset > 0) {
-          let newDate = firebase.firestore.Timestamp.fromDate( new Date(dbDate.add(offset, 'w').format('YYYY-MM-DDTHH:mm:ss')))
-          doc.ref.update({'date': newDate});
-          // increment role ID once
-          usersC.orderBy('points', 'desc').get()
-            .then(snapshot => {
-              snapshot.forEach(doc => {
-                var newID = (doc.data().role + 1) % 5;
-                doc.ref.update({'role': newID});
-              });
-            })
-            .catch(err => {
-              console.log('Error getting role document', err);
-            });
-          //realtime update people array
-          usersC.onSnapshot(querySnapshot => {
-            var newArray = [];
-            querySnapshot.forEach(userDoc => {
-              newArray.push(userDoc.data());
-              this.setState({people:newArray});
-            });
-          }, err => {
-            console.log(`Encountered error: ${err}`);
-          });
-
-        } else {
-          //just get people
-          usersC.onSnapshot(querySnapshot => {
-            var newArray = [];
-            querySnapshot.forEach(doc => {
-              newArray.push(doc.data());
-              this.setState({people:newArray})
-            });
-          }, err => {
-            console.log(`Encountered error: ${err}`);
-          });
-        }
-    })
-    .catch(err => {
-      console.log('Error getting date document', err);
-    })
-  }
-  render() {
-    return (
-      <div>
-        <div className="sticky">
-            <h1 className="title">Chore Wars</h1>
-        </div>
-        <div className="home">
-          <div className="row">
-            <div className="left">
-              <h2>Leaderboard</h2>
-              <Leaderboard people={this.state.people} />
-            </div>
-            <div className="right">
-              <h2>Roles</h2>
-              <Roles people={this.state.people} roles={this.state.roles}/>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+		dateC
+			.get()
+			.then((doc) => {
+				//account for timezone offset
+				var dbDate = moment('01/01/1970 00:00:00', 'DD/MM/YYYY HH:mm:ss')
+					.seconds(doc.data().date.seconds)
+					.add(-5, 'h');
+				var ms = moment().diff(dbDate);
+				var offset = Math.floor(moment.duration(ms)._data.days / 7);
+				if (offset > 0) {
+					let newDate = firebase.firestore.Timestamp.fromDate(
+						new Date(dbDate.add(offset, 'w').format('YYYY-MM-DDTHH:mm:ss'))
+					);
+					doc.ref.update({ date: newDate });
+					// increment role ID once
+					usersC
+						.orderBy('points', 'desc')
+						.get()
+						.then((snapshot) => {
+							snapshot.forEach((doc) => {
+								var newID = (doc.data().role + 1) % 5;
+								doc.ref.update({ role: newID });
+							});
+						})
+						.catch((err) => {
+							console.log('Error getting role document', err);
+						});
+					//realtime update people array
+					usersC.onSnapshot(
+						(querySnapshot) => {
+							var newArray = [];
+							querySnapshot.forEach((userDoc) => {
+								newArray.push(userDoc.data());
+								this.setState({ people: newArray });
+							});
+						},
+						(err) => {
+							console.log(`Encountered error: ${err}`);
+						}
+					);
+				} else {
+					//just get people
+					usersC.onSnapshot(
+						(querySnapshot) => {
+							var newArray = [];
+							querySnapshot.forEach((doc) => {
+								newArray.push(doc.data());
+								this.setState({ people: newArray });
+							});
+						},
+						(err) => {
+							console.log(`Encountered error: ${err}`);
+						}
+					);
+				}
+			})
+			.catch((err) => {
+				console.log('Error getting date document', err);
+			});
+	}
+	render() {
+		return (
+			<div>
+				<div className="sticky">
+					<h1 className="title">Chore Wars</h1>
+				</div>
+				<div className="home">
+					<div className="row">
+						<div className="left">
+							<h2>Leaderboard</h2>
+							<Leaderboard people={this.state.people} />
+						</div>
+						<div className="right">
+							<h2>Roles</h2>
+							<Roles people={this.state.people} roles={this.state.roles} />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-  export default Home;
+export default Home;
--- src/components/Roles.js
+++ src/components/Roles.js
@@ -1,86 +1,101 @@
-import React,  {Component} from 'react';
-import Role from './Role'
+import React, { Component } from 'react';
+import Role from './Role';
 import db from '../firebaseconfig';
 
 const moment = require('moment');
 const firebase = require('firebase/app');
 
+const USERCOUNT = 5;
+
 var bathC = db.collection('general').doc('bathroom');
 
 class Roles extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      uBaron: {
-        name: 'Upstairs Baron',
-        description: 'The Bathroom Baron for the upstairs boys. Must clean the bathroom once in the 2 week span.',
-      },
-      dBaron: {
-        name: 'Downstairs Baron',
-        description: 'The Bathroom Baron for the downstairs boys. Must clean the bathroom once in the 2 week span.',
-      },
-      uID: 0,
-      dID: 0
-    };
-    this.toggleRoles = this.toggleRoles.bind(this);
+	constructor(props) {
+		super(props);
+		this.state = {
+			uBaron: {
+				name: 'Upstairs Baron',
+				description:
+					'The Bathroom Baron for the upstairs boys. Must clean the bathroom once in the 2 week span.',
+			},
+			dBaron: {
+				name: 'Downstairs Baron',
+				description:
+					'The Bathroom Baron for the downstairs boys. Must clean the bathroom once in the 2 week span.',
+			},
+			uID: 0,
+			dID: 0,
+		};
+		this.toggleRoles = this.toggleRoles.bind(this);
 
+		bathC
+			.get()
+			.then((doc) => {
+				var dbDate = moment(
+					'01/01/1970 00:00:00',
+					'DD/MM/YYYY HH:mm:ss'
+				).seconds(doc.data().date.seconds);
+				var ms = moment().diff(dbDate);
+				var offset = Math.floor(moment.duration(ms)._data.days / 7);
+				if (offset > 1) {
+					// let newDate = firebase.firestore.Timestamp.fromDate( new Date(dbDate.add(offset, 'w').format('YYYY-MM-DDTHH:mm:ss')))
+					let newDate = firebase.firestore.Timestamp.fromDate(
+						new Date(dbDate.add(offset, 'w').unix())
+					);
+					doc.ref.update({ date: newDate });
+					// increment bathroom var
+					var newUp = ((doc.data().upstairs + 1) % 3) + 3;
+					var newDown = (doc.data().downstairs + 1) % 3;
+					doc.ref.update({ upstairs: newUp });
+					doc.ref.update({ downstairs: newDown });
+					this.setState({ uID: newUp });
+					this.setState({ dID: newDown });
+				} else {
+					this.setState({ uID: doc.data().upstairs });
+					this.setState({ dID: doc.data().downstairs });
+				}
+			})
+			.catch((err) => {
+				console.log('Error getting bathroom document', err);
+			});
+	}
 
-    bathC.get()
-      .then(doc => {
-        var dbDate = moment("01/01/1970 00:00:00","DD/MM/YYYY HH:mm:ss").seconds(doc.data().date.seconds);
-        var ms = moment().diff(dbDate);
-        var offset = Math.floor(moment.duration(ms)._data.days/7);
-        if (offset > 1) {
-          // let newDate = firebase.firestore.Timestamp.fromDate( new Date(dbDate.add(offset, 'w').format('YYYY-MM-DDTHH:mm:ss')))
-          let newDate = firebase.firestore.Timestamp.fromDate( new Date( dbDate.add(offset, 'w').unix() ))
-          doc.ref.update({'date': newDate});
-          // increment bathroom var
-          var newUp = (doc.data().upstairs + 1) % 3 + 3;
-          var newDown = (doc.data().downstairs + 1) % 3;
-          doc.ref.update({'upstairs': newUp});
-          doc.ref.update({'downstairs': newDown});
-          this.setState({uID: newUp});
-          this.setState({dID: newDown})
-        } else {
-          this.setState({uID: doc.data().upstairs});
-          this.setState({dID: doc.data().downstairs});
-        }
-    })
-    .catch(err => {
-      console.log('Error getting bathroom document', err);
-    })
-  }
+	toggleRoles = (index) => {
+		if (this.state.show[0]) {
+			this.setState((prevState) => ({
+				// show: prevState.slice(0,index).concat([true]).concat(prevState.slice(index+1, prevState.length))
+				show: new Array(USERCOUNT).fill(false),
+			}));
+		} else {
+			this.setState((prevState) => ({
+				// show: prevState.slice(0,index).concat([false]).concat(prevState.slice(index+1, prevState.length))
+				show: new Array(USERCOUNT).fill(true),
+			}));
+		}
+	};
 
-  toggleRoles = (index) => {
-    if(this.state.show[0]) {
-      this.setState(prevState => ({
-        // show: prevState.slice(0,index).concat([true]).concat(prevState.slice(index+1, prevState.length))
-        show: [false, false, false, false, false ]
-      }))
-    } else {
-      this.setState(prevState => ({
-        // show: prevState.slice(0,index).concat([false]).concat(prevState.slice(index+1, prevState.length))
-        show: [true, true, true, true, true ]
-      }))
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <table className="roles">
-          <tbody>
-            { [].concat(this.props.people)
-              .sort(function compare(a, b) {
-                if (a.role < b.role) return -1;
-                if (a.role > b.role) return  1;
-                return 0;
-              })
-              .map(p =>
-                <Role key={p.id} role={this.props.roles[p.role].name} desc={this.props.roles[p.role].description} name={p.name}></Role>
-              )
-          }
-            { /*
+	render() {
+		console.log(this.props.roles);
+		return (
+			<div>
+				<table className="roles">
+					<tbody>
+						{[]
+							.concat(this.props.people)
+							.sort(function compare(a, b) {
+								if (a.role < b.role) return -1;
+								if (a.role > b.role) return 1;
+								return 0;
+							})
+							.map((p) => (
+								<Role
+									key={p.id}
+									role={this.props.roles[p.role].name}
+									desc={this.props.roles[p.role].description}
+									name={p.name}
+								></Role>
+							))}
+						{/*
               }{ [].concat(this.props.people).map(p =>
                 p.id === this.state.uID &&
                 <Role key={0} class="divider" role={this.state.uBaron.name} desc={this.state.uBaron.description} name={p.name}></Role>
@@ -90,13 +105,11 @@ class Roles extends Component {
                 <Role key={0} role={this.state.dBaron.name} desc={this.state.dBaron.description} name={p.name}></Role>
               )}
             */}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
-
-
 
 export default Roles;
