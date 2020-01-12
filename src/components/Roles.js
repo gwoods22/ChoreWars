1bbc19c7418bf5a64ae0046131e5298752bc75de
mcdonl1 <lucmcd10@gmail.com>
2020-01-12 18:02:50 -0500

    Changed roles array to be stored in db

--- src/components/Roles.js
+++ src/components/Roles.js
@@ -5,8 +5,6 @@ import db from '../firebaseconfig';
 const moment = require('moment');
 const firebase = require('firebase/app');
 
-const USERCOUNT = 5;
-
 var bathC = db.collection('general').doc('bathroom');
 
 class Roles extends Component {
@@ -25,6 +23,7 @@ class Roles extends Component {
 			},
 			uID: 0,
 			dID: 0,
+			userCount: props.people.length,
 		};
 		this.toggleRoles = this.toggleRoles.bind(this);
 
@@ -64,36 +63,36 @@ class Roles extends Component {
 		if (this.state.show[0]) {
 			this.setState((prevState) => ({
 				// show: prevState.slice(0,index).concat([true]).concat(prevState.slice(index+1, prevState.length))
-				show: new Array(USERCOUNT).fill(false),
+				show: new Array(this.state.userCount).fill(false),
 			}));
 		} else {
 			this.setState((prevState) => ({
 				// show: prevState.slice(0,index).concat([false]).concat(prevState.slice(index+1, prevState.length))
-				show: new Array(USERCOUNT).fill(true),
+				show: new Array(this.state.userCount).fill(true),
 			}));
 		}
 	};
-
 	render() {
 		return (
 			<div>
 				<table className="roles">
 					<tbody>
-						{[]
-							.concat(this.props.people)
-							.sort(function compare(a, b) {
-								if (a.role < b.role) return -1;
-								if (a.role > b.role) return 1;
-								return 0;
-							})
-							.map((p) => (
-								<Role
-									key={p.id}
-									role={this.props.roles[p.role].name}
-									desc={this.props.roles[p.role].description}
-									name={p.name}
-								></Role>
-							))}
+						{this.props.roles && this.props.people
+							? this.props.people
+									.sort(function compare(a, b) {
+										if (a.role < b.role) return -1;
+										if (a.role > b.role) return 1;
+										return 0;
+									})
+									.map((p) => (
+										<Role
+											key={p.id}
+											role={this.props.roles[p.role].name}
+											desc={this.props.roles[p.role].description}
+											name={p.name}
+										/>
+									))
+							: null}
 						{/*
               }{ [].concat(this.props.people).map(p =>
                 p.id === this.state.uID &&
